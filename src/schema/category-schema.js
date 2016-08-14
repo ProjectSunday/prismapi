f33dd6f640b065f3875ab14d1374d8699a942d06
Hai Nguyen <hai5nguy@gmail.com>
2016-08-14 19:09:49 -0400

    remove create category

--- src/schema/category-schema.js
+++ src/schema/category-schema.js
@@ -23,29 +23,29 @@ const queries = {
 
 const mutations = {
 
-	createCategory: {
-		type: CategoryType,
-		args: {
-			//token: //shuld be here
-			name: { type: new GraphQLNonNull(GraphQLString) }
-		},
-		resolve: async (root, args) => {
-			var context = new Context()
-			return context.category.create({ name: args.name })
-		}
-	},
+	// createCategory: {
+	// 	type: CategoryType,
+	// 	args: {
+	// 		//token: //shuld be here
+	// 		name: { type: new GraphQLNonNull(GraphQLString) }
+	// 	},
+	// 	resolve: async (root, args) => {
+	// 		var context = new Context()
+	// 		return context.category.create({ name: args.name })
+	// 	}
+	// },
 
-	deleteCategory: {
-		type: CategoryType,
-		args: {
-			// token: //should be here
-			_id: { type: new GraphQLNonNull(GraphQLID) }
-		},
-		resolve: async (root, args) => {
-			var context = new Context()
-			return await context.category.delete(args._id)
-		}
-	}
+	// deleteCategory: {
+	// 	type: CategoryType,
+	// 	args: {
+	// 		// token: //should be here
+	// 		_id: { type: new GraphQLNonNull(GraphQLID) }
+	// 	},
+	// 	resolve: async (root, args) => {
+	// 		var context = new Context()
+	// 		return await context.category.delete(args._id)
+	// 	}
+	// }
 
 }
 
